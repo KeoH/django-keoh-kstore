3de8c6cf20ab7070d49e9831b29a7cb9a2af7d1f
Francisco Manzano <keoh77@gmail.com>
2016-03-03 16:46:45 +0100

    Soporte para Themes ampliado

--- kstore/models/config.py
+++ kstore/models/config.py
@@ -4,5 +4,9 @@ from django.db import models
 class BasicConfiguration(models.Model):
     company_name = models.CharField('Nombre de la empresa', max_length=255)
     theme = models.CharField('Theme', max_length=255, default="one")
+
+    def get_theme_url(self):
+        return 'themes/'+ self.theme
+
     def __unicode__(self):
         return self.company_name + ' configuration'
