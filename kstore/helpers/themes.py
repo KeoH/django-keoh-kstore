825d7984a06823a4e592265c4e791b455ddbb481
Francisco Manzano <keoh77@gmail.com>
2016-03-19 00:43:59 +0100

    Funciones auxiliares para los temas

--- kstore/helpers/themes.py
+++ kstore/helpers/themes.py
@@ -1,4 +1,4 @@
 from kstore.models import BasicConfiguration
 
 def get_theme_url():
-    return BasicConfiguration.objects.all().first().get_theme_url()
+    return BasicConfiguration.objects.first().get_theme_url()
